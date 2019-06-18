0656e0925e9a6ab49d32a49f8c4e22b5bcea6b88
Jean-Milost Reymond <jean_milost@hotmail.com>
2019-06-18 00:37:26 -0300

    - added: key with highlight system in survival demo scene (NOT TERMINATED) - deleted: useless and no longer used file

---
+++ Survival Demo Scene/Assets/GameCore/Scripts/GS_Key.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GS_Key : MonoBehaviour
+{
+    private Plane m_KeyHighLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_KeyHighLight = GetComponentInChildren<Plane>();
+        //Debug.Assert(m_KeyHighLight != null);
+//m_KeyHighLight.
+    }
+}
