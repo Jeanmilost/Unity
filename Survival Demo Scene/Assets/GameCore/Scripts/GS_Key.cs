d314a8b42016c9f0be9ac995fe213100ef0c46c0
Jeanmilost <jean_milost@hotmail.com>
2019-06-19 00:35:53 -0300

    - added: key with highlight, and now player may take the key

--- Survival Demo Scene/Assets/GameCore/Scripts/GS_Key.cs
+++ Survival Demo Scene/Assets/GameCore/Scripts/GS_Key.cs
@@ -1,22 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/**
+* This class manages the collectible key object
+* @author Jean-Milost Reymond
+*/
 public class GS_Key : MonoBehaviour
 {
-    private Plane m_KeyHighLight;
+    public float m_KeyHighlightVelocity  = 5.0f;
+    public float m_KeyHighlightSleepTime = 2.5f;
+
+    private GameObject m_Key;
+    private GameObject m_KeyHighLight;
+    private Renderer   m_KeyHighLightRenderer;
+    private float      m_KeyHighlightAlpha  = 0.0f;
+    private float      m_KeyHighlightOffset = 1.0f;
+    private float      m_KeyHighlightSleepTimeStamp;
+    private bool       m_DoWait;
+
+    public bool IsKept { get; set; } = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Key = GameObject.Find("Key");
+        Debug.Assert(m_Key);
+
+        m_KeyHighLight = GameObject.Find("Key_Highlight");
+        Debug.Assert(m_KeyHighLight);
+
+        m_KeyHighLightRenderer = m_KeyHighLight.GetComponent<Renderer>();
+        Debug.Assert(m_KeyHighLightRenderer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_KeyHighLight = GetComponentInChildren<Plane>();
-        //Debug.Assert(m_KeyHighLight != null);
-//m_KeyHighLight.
+        if (!m_DoWait)
+        {
+            m_KeyHighlightAlpha += (m_KeyHighlightVelocity * Time.deltaTime * m_KeyHighlightOffset);
+
+            if (m_KeyHighlightAlpha <= 0.0f)
+            {
+                m_KeyHighlightAlpha          = 0.0f;
+                m_KeyHighlightSleepTimeStamp = Time.time;
+                m_DoWait                     = true;
+            }
+            else
+            if (m_KeyHighlightAlpha >= 1.0f)
+            {
+                m_KeyHighlightAlpha  =  1.0f;
+                m_KeyHighlightOffset = -m_KeyHighlightOffset;
+            }
+        }
+        else
+        if (Time.time - m_KeyHighlightSleepTimeStamp >= m_KeyHighlightSleepTime)
+        {
+            m_KeyHighlightOffset = -m_KeyHighlightOffset;
+            m_DoWait             =  false;
+        }
+
+        m_KeyHighLightRenderer.material.color = new Color(m_KeyHighLightRenderer.material.color.r,
+                                                          m_KeyHighLightRenderer.material.color.g,
+                                                          m_KeyHighLightRenderer.material.color.b,
+                                                          m_KeyHighlightAlpha);
+    }
+
+    /**
+    * Called while the character stays inside a trigger zone
+    *@param other - trigger zone collider in which the character is staying
+    */
+    void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Laure" && Input.GetKeyUp(KeyCode.Space))
+        {
+            m_Key.SetActive(false);
+            IsKept = true;
+        }
     }
 }
