9cccef3d6a29771d4493beab5636983c4e7378dd
Jean-Milost Reymond <jean_milost@hotmail.com>
2020-07-22 22:50:35 -0300

    - added: first person view demo

---
+++ 1st Person View/Assets/Engine/Scripts/WS_SoundController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+/**
+* Sound controller, contains the sounds the player may emit while he's moving
+*/
+public class WS_SoundController
+{
+    public AudioSource[] m_AudioSources;
+
+    /**
+    * Initializes the class
+    *@param audioSources - audio sources the player contains
+    */
+    public void Init(Component[] audioSources)
+    {
+        m_AudioSources = new AudioSource[audioSources.Length];
+
+        // get the children audio sources
+        for (int i = 0; i < audioSources.Length; ++i)
+            // get the audio source
+            m_AudioSources[i] = audioSources[i] as AudioSource;
+    }
+
+    /**
+    * Checks if a sound can be played
+    *@param key - the sound key to check
+    *@return true if the sound can be played, otherwise false
+    */
+    public bool CanPlay(int key)
+    {
+        // is type out of bounds
+        if (m_AudioSources.Length <= key)
+            return false;
+
+        // check the sound content
+        return (m_AudioSources[key] != null);
+    }
+
+    /**
+    * Plays a sound
+    *@param key - the sound key to play
+    */
+    public void Play(int key)
+    {
+        // can play the sound?
+        if (!CanPlay(key))
+            return;
+
+        // is sound already playing?
+        if (m_AudioSources[key].isPlaying)
+            return;
+
+        // play the sound
+        m_AudioSources[key].Play();
+    }
+
+    /**
+    * Stops to play a sound
+    *@param type - the sound key to play
+    */
+    public void Stop(int key)
+    {
+        // can play the sound?
+        if (!CanPlay(key))
+            return;
+
+        // is sound already playing?
+        if (!m_AudioSources[key].isPlaying)
+            return;
+
+        // play the sound
+        m_AudioSources[key].Stop();
+    }
+
+    /**
+    * Gets the sound
+    *@param type - the sound to get
+    *@return the sound, null if not found or on error
+    */
+    public AudioSource GetSound(int key)
+    {
+        // is type out of bounds
+        if (m_AudioSources.Length <= key)
+            return null;
+
+        // play the sound
+        return m_AudioSources[key];
+    }
+}
---
+++ 1st Person View/README.md
@@ -0,0 +1,6 @@
+# First person view
+This project demonstrates a possible solution for a first person view with an attached animated model. The scene contains a mirror, and the body is visible when the player lowers his head.
+
+I noticed several side effects, however, because I needed to place the camera close to the torso instead of the head to workaround a bad clipping on the body. The consequence is that the proportions of the character are exaggerated compared to the reality, and this distortion may be visible in some circumstances, e.g when the character is close to the mirror.
+
+<img src="Screenshots/1stPersonView.png" width="500" />
